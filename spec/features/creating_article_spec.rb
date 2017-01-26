ede31d5e6b1f95809e853ba7d9d5214f4b37c67a
SHERRY <swasieko@gmail.com>
2017-01-26 19:15:51 +0000

    Fix broken editing  article spec

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.feature "Creating Articles" do
   before do 
-    @john = User.create(email: "john@example.com", password: "password")
+    @john = User.create!(email: "john@example.com", password: "password")
     login_as(@john)
   end
   
