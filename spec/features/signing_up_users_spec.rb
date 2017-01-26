d59f24a5d5a7c493fda816e750262f0025a93847
SHERRY <swasieko@gmail.com>
2017-01-26 06:32:25 +0000

    Setup user sign in

--- spec/features/signing_up_users_spec.rb
+++ spec/features/signing_up_users_spec.rb
@@ -6,8 +6,8 @@ RSpec.feature "signup users" do
     visit "/"
     click_link "Sign up"
     fill_in "Email", with: "user@example.com"
-    fill_in "password", with: "password"
-    fill_in "password_confirmation", with: "password"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
     click_button "Sign up"
     
     expect(page).to have_content("You have signed up successfully")
@@ -18,8 +18,8 @@ RSpec.feature "signup users" do
     visit "/"
     click_link "Sign up"
     fill_in "Email", with: ""
-    fill_in "password", with: ""
-    fill_in "password_confirmation", with: ""
+    fill_in "Password", with: ""
+    fill_in "Password confirmation", with: ""
     click_button "Sign up"
     
    # expect(page).to have_content("You have not signed up successfully")
